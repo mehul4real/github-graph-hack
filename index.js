280c170db77cd8e50cefd0052cd43418f64c560d
Mehul Saxena <mehul@indiagold.co>
2023-03-27 22:01:52 +0530

    package update

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ const makeCommit = n => {
     const x = random.int(0,54)
     const y = random.int(0,6)
 
-    const DATE = moment().subtract(1,'y').add(1,'d')
-        .add(x,'w').add(y,'d').format()
+    const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format()
 
     const data = {
         date: DATE
     }
-    
+
     console.log(DATE)
     
     jsonfile.writeFile(FILE_PATH, data, ()=>{
